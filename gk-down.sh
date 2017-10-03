10d2f4a43b966e3477d84425332a2ada0e357e4a
Jon Cope <jcope@redhat.com>
2017-10-03 18:01:43 -0500

    Fix gk-down paths after restructure

--- gk-down.sh
+++ gk-down.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 set -euo pipefail
-REPO_ROOT="$(realpath $(dirname $0)/../../)"
+REPO_ROOT="$(realpath $(dirname $0))"
 
-source $REPO_ROOT/deploy/cluster/lib/config.sh
-source $REPO_ROOT/deploy/cluster/lib/util.sh
+source $REPO_ROOT/lib/config.sh
+source $REPO_ROOT/lib/util.sh
 
 util::check-orphaned-resources
 util::do-cleanup
