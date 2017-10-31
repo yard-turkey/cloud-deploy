f73ad58986d17e991ac7902eababdbc0db445c0d
Jon Cope <jcope@redhat.com>
2017-10-31 12:10:57 -0500

    Added --zone to all ssh/scp commands, fixed build-topology.sh.  Now generates a new topology each run.

--- lib/util.sh
+++ lib/util.sh
@@ -1,15 +1,5 @@
 #! /bin/bash
 
-# Generate a 4 character string to be used as a suffix for
-# GCP resources.
-# Returns: Array of stings.
-# Args:
-#	1 - int, number of unique suffixes required.
-function util::unique_id_arr {
-	local num_ids=${1:-"4"}
-	UUIDS="$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 4 | head -n $num_ids)"
-}
-
 # pre-start-check looks for existing resources that may have been orphaned
 # from a prior run.
 # args:
