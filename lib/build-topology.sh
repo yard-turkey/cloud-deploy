51366d90123509686805b819ab5342b1804c5efd
Jon Cope <jcope@redhat.com>
2017-12-07 22:00:08 -0600

    fixed bug in build-topology (reversed node name and ip)

--- lib/build-topology.sh
+++ lib/build-topology.sh
@@ -11,6 +11,6 @@ REPO_ROOT="$(realpath $(dirname ../../))"
 source "$REPO_ROOT"/lib/config.sh
 source "$REPO_ROOT"/lib/util.sh
 
-HOSTS=($(gcloud compute instances list --regexp="$GK_NODE_NAME.*" | awk 'NR>1{ printf "%-30s%s\n", $1, $4}'))
+HOSTS=($(gcloud compute instances list --regexp="$GK_NODE_NAME.*" | awk 'NR>1{ printf "%-30s%s\n", $4, $1}'))
 TOPO_PATH="$(util::gen_gk_topology \"${HOSTS[*]}\")"
 printf "Successfully generated a new topology file: %s\n" "$TOPO_PATH"
